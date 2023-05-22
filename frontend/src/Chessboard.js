ced45b4a05c4bce755444cad10b33cb52b2941bd
Manan Vaishnav <manan071104@gmail.com>
2023-05-22 18:46:47 +0530

    Added Chessboard structure component

---
+++ frontend/src/Chessboard.js
@@ -0,0 +1,91 @@
+import React from 'react';
+function Chessboard() {
+    return (
+      <div className='Chessboard'>
+       <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+        <div className='row'>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+        </div>
+
+      </div>
+    );
+  }
+  
+  export default Chessboard;
+  
